3ce8d6f217c53655bb43c793d05d3fa6f3c3b491
unknown <jaylord.abueva98@gmail.com>
2020-04-22 10:31:56 +0800

    Jay First Commit

---
+++ GlobalVariableScope/Activities.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace GlobalVariableScope
+{
+    public class NewVariable : CodeActivity
+    {
+        data my_data = data.Instance;
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> VariableName { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<object> VariableValue { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            String CreateVariableKey = VariableName.Get(context);
+            object CreateVariableValue = VariableValue.Get(context);
+            my_data.AddData(CreateVariableKey, CreateVariableValue);
+        }
+    }
+    public class RetrieveVariable : CodeActivity
+    {
+        data my_data = data.Instance;
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> VariableName { get; set; }
+
+        [Category("Output")]
+        public OutArgument<object> VariableOutput { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            String CreateVariableKey = VariableName.Get(context);
+            VariableOutput.Set(context, my_data.RetrieveData(CreateVariableKey));
+        }
+    }
+    public class DisposeVariable : CodeActivity
+    {
+        data my_data = data.Instance;
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> VariableName { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            String CreateVariableKey = VariableName.Get(context);
+            my_data.DeleteData(CreateVariableKey);
+        }
+    }
+    public class UpdateVariable : CodeActivity
+    {
+        data my_data = data.Instance;
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<String> VariableName { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<object> VariableNewValue { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            String CreateVariableKey = VariableName.Get(context);
+            object CreateVariableNewValue = VariableNewValue.Get(context);
+            my_data.UpdateData(CreateVariableKey, CreateVariableNewValue);
+        }
+    }
+    public class PrintKey : CodeActivity
+    {
+        data my_data = data.Instance;
+        [Category("Output")]
+        public OutArgument<object> VariableOutput { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            VariableOutput.Set(context, my_data.PrintData());
+        }
+    }
+}
---
+++ GlobalVariableScope/data.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalVariableScope
+{
+    public sealed class data
+    {
+        Dictionary<String, object> data_dict = new Dictionary<String, object>();
+        private static data instance = null;
+        public static data Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new data();
+                }
+                return instance;
+            }
+        }
+        public void AddData(String key, object value)
+        {
+            if (data_dict.ContainsKey(key))
+            {
+                throw new Exception("Key already added");
+            } else {
+                data_dict.Add(key, value);
+            }
+        }
+        public object RetrieveData(String key)
+        {
+            if (data_dict.ContainsKey(key) == false)
+            {
+                throw new Exception("Key don't exist");
+            }
+
+            return data_dict[key];
+        }
+        public void DeleteData(String key)
+        {
+            data_dict.Remove(key);
+        }
+        public void UpdateData(String key, object NewData)
+        {
+            data_dict[key] = NewData;
+        }
+        public String PrintData()
+        {
+            String text = null;
+            foreach (KeyValuePair<String, object> kvp in data_dict)
+            {
+                text = text+","+kvp.Key;
+            }
+            return text;
+        }
+    }
+}
